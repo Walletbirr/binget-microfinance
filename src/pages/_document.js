acc816f22a5acd9b27e396cf49e6c7fe7d78a1b3
Yeabsira Tesfaye <yeabsira.tesfaye@wallet.com>
2025-11-19 16:20:20 +0300

    fav icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="shortcut icon" href="/static/logo.ico" />
       <Head />
       <body className="antialiased">
         <Main />
